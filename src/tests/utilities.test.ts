2f7a611c31e79b43ce558bcae8809106e706da6d
Jeet Singh <jeetsingh@gmail.com>
2021-05-11 15:27:58 -0400

    feat(sql generator): added search sql generator

---
+++ src/tests/utilities.test.ts
@@ -0,0 +1,67 @@
+import { generate_sql_statement } from '../utils/utilities'
+
+const my_object_full = {
+    name: 'jeet',
+    createDate: { lt: '2020-01-01' },
+    employees: { lte: '100' },
+    isPublic: 'true'
+};
+const my_object_full_sql = 'select name, createDate, employees, isPublic from organizations where name like %$1% and createDate < $2 and employees <= $3 and isPublic = $4 '
+
+const my_object_name = {
+    name: 'jeet'
+}
+const my_object_name_sql = 'select name, createDate, employees, isPublic from organizations where name like %$1% '
+
+const my_object_create = {
+    createDate: '2020-02-02',
+    employees: '100'
+}
+const my_object_create_sql = 'select name, createDate, employees, isPublic from organizations where createDate = $1 and employees = $2 '
+
+const my_object_create_extra_value = {
+    createDate: '2020-02-02',
+    extra: '9001',
+    employees: '100'
+}
+
+const my_object_full_gt = {
+    name: 'jeet',
+    createDate: { gt: '2020-01-01' },
+    employees: { gte: '100' },
+    isPublic: 'true'
+};
+const my_object_full_gt_sql = 'select name, createDate, employees, isPublic from organizations where name like %$1% and createDate > $2 and employees >= $3 and isPublic = $4 '
+
+describe('sqlStatementGenerator', () => {
+    it('should return the correct sql statement for the passed in full lt queries', async () => {
+        const generated_sql = await generate_sql_statement(my_object_full)
+        expect.assertions(1)
+        expect(generated_sql).toEqual(my_object_full_sql)
+    });
+    it('should return the correct sql statement for the passed in with full gt queries', async () => {
+        const generated_sql = await generate_sql_statement(my_object_full_gt)
+        expect.assertions(1)
+        expect(generated_sql).toEqual(my_object_full_gt_sql)
+    });
+    it('should return the correct sql statement for the passed in half queries', async () => {
+        const generated_sql = await generate_sql_statement(my_object_create)
+        expect.assertions(1)
+        expect(generated_sql).toEqual(my_object_create_sql)
+    });
+    it('should return the correct sql statement for the passed in half queries with extra', async () => {
+        const generated_sql = await generate_sql_statement(my_object_create_extra_value)
+        expect.assertions(1)
+        expect(generated_sql).toEqual(my_object_create_sql)
+    });
+    it('should return the correct sql statement for the passed in one query', async () => {
+        const generated_sql = await generate_sql_statement(my_object_name)
+        expect.assertions(1)
+        expect(generated_sql).toEqual(my_object_name_sql)
+    });
+    it('should return the correct sql statement for the passed in no queries', async () => {
+        const generated_sql = await generate_sql_statement({})
+        expect.assertions(1)
+        expect(generated_sql).toEqual('select name, createDate, employees, isPublic from organizations ')
+    });
+})
--- src/utils/utilities.ts
+++ src/utils/utilities.ts
@@ -2,9 +2,40 @@ import { Request } from 'express';
 
 class ValidationError extends Error {
     constructor(message: string) {
-        super(message); // (1)
-        this.name = "ValidationError"; // (2)
+        super(message);
+        this.name = "ValidationError";
     }
+}
+
+async function generate_sql_statement(queries: Request["query"]): Promise<String> {
+    let sql_stmt = "select name, createDate, employees, isPublic from organizations "
+    if (Object.keys(queries).length == 0) {
+        return Promise.resolve(sql_stmt);
+    } else {
+        sql_stmt += "where ";
+        let i = 0
+        for (const key in queries) {
+            const value = queries[key]
+            i++;
+            if (i > 1) { if (!(sql_stmt.endsWith("and "))) { sql_stmt += "and " } };
+            if (key == 'name') { sql_stmt += `name like %$${i}% `; continue;
+            } else if (key == "createDate") { sql_stmt += "createDate ";
+            } else if (key == "employees") { sql_stmt += "employees ";
+            } else if (key == "isPublic") { sql_stmt += `isPublic = $${i} `; continue;
+            } else { i--; continue; }
+            
+            if (typeof value != "string") {
+                for (const superkey in value) {
+                    if (superkey == "lt") { sql_stmt += `< $${i} `; continue;
+                    } else if (superkey == "lte") { sql_stmt += `<= $${i} `; continue;
+                    } else if (superkey == "gt") {sql_stmt += `> $${i} `; continue;
+                    } else if (superkey == "gte") {sql_stmt += `>= $${i} `; continue; 
+                    } else { continue }  
+                }
+            } else { sql_stmt += `= $${i} `}
+        }
+    }
+    return Promise.resolve(sql_stmt)
 }
 
 async function validate_request(req: Request): Promise<void> {
@@ -22,4 +53,4 @@ async function validate_request(req: Request): Promise<void> {
     };
 }
 
-export { validate_request, ValidationError }
+export { validate_request, generate_sql_statement,ValidationError }
