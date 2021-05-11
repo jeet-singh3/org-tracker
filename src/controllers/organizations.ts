893c468af8935f87be8cadc763ba573f894f5e20
Jeet Singh <jeetsingh@gmail.com>
2021-05-11 10:58:33 -0400

    feat(db re-org): moved db into own utils file and importing

--- src/controllers/organizations.ts
+++ src/controllers/organizations.ts
@@ -1,16 +1,6 @@
 import { Request, Response } from 'express';
-import { Pool } from 'pg'
 import { validate_request, ValidationError } from '../utils/utilities'
-
-const pool = new Pool({
-    host: process.env.DB_HOST || "localhost",
-    port: parseInt(process.env.DB_PORT || "10001", 10) || 10001,
-    user: process.env.DB_USER || "postgres",
-    password: process.env.DB_PASS || "postgres",
-    max: 20,
-    idleTimeoutMillis: 30000,
-    connectionTimeoutMillis: 2000,
-})
+import { pool } from '../utils/dbPool'
 
 const org_create = async (req: Request, res: Response) => {
     console.log('Organization Create endpoint hit!');
